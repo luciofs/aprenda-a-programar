0d6113a5f090329a9999c9d0f9f05927a7a84013
Lucio Flavio <lucioflasan@gmail.com>
2022-08-24 14:59:28 -0300

    trabalhando com variaveis no arquivo pratica-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email
 let profissao
 let idade
 let local
+nome = "Lucio"
+email = "lucioflasan@gmail.com"
+profissao = "Analista de Ti"
+idade = 49
+local = "Paris"
+console.log (nome, email, profissao, idade, local)
