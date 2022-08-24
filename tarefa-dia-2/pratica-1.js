29fd487822211dd4d508376f251f1c06657b83fb
Lucio Flavio <lucioflasan@gmail.com>
2022-08-24 15:17:30 -0300

    finalizando a pratica com variaveis no arquivo pratica-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,10 +8,10 @@ email = "lucioflasan@gmail.com"
 profissao = "Analista de Ti"
 idade = 49
 local = "Paris"
-console.log ("Olá ", nome,"!")
+console.log ("Olá ",nome,"!")
 console.log ("Somos aqui da agência de turismo Viagem dos Sonhos.")
-console.log ("Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ",local, ".")
+console.log ("Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ",local,".")
 console.log ("Confirma pra gente alguns dados:") 
-console.log ("A sua idade é ",idade, ", você é ",profissao, "e o seu email é ",email," ?")
+console.log ("A sua idade é ",idade,", você é ",profissao," e o seu email é ",email,"?")
 console.log ("Ficamos no aguardo. Muito obrigado e boa viagem!")
     
